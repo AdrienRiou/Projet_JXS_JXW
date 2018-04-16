113829f740cc343b5a2bbca6f026b72363b790f3
AcidBaz <basile.chiquet@hotmail.fr>
2018-04-16 17:06:49 +0200

    added 3 components of the main page

--- front/jxs-front/src/app/app.module.ts
+++ front/jxs-front/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { LateralPanelComponent } from './lateral-panel/lateral-panel.component';
+import { FilesDisplayComponent } from './files-display/files-display.component';
+import { FileInfoComponent } from './file-info/file-info.component';
 
+
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LateralPanelComponent,
+    FilesDisplayComponent,
+    FileInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
