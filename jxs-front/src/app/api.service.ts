5d19b4b92a6a6afc7ee4ff4503b480e23ce0dd5c
Adrien Riou <adr.riou@gmail.com>
2018-04-24 10:41:35 +0200

    front small update

--- jxs-front/src/app/api.service.ts
+++ jxs-front/src/app/api.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 
-import {Observable} from "rxjs/Observable";
+
 import {HttpClient} from "@angular/common/http";
 @Injectable()
 export class ApiService {
 
   constructor(private http : HttpClient) { }
 
-  public connectAPI(url : String) {
+  public connectAPI(url : string) {
+    var res = false
     this.http.get(url).subscribe( data => {
       console.log(data);
-    })
+    });
+    return res;
   }
 
 }
--- jxs-front/src/app/lateral-panel/lateral-panel.component.ts
+++ jxs-front/src/app/lateral-panel/lateral-panel.component.ts
@@ -7,16 +7,16 @@ import { ApiService} from "../api.service";
 })
 export class LateralPanelComponent implements OnInit {
 
-  google_auth : boolean = false;
-//  const google_url : String = "https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&response_type=code&client_id=752520275648-e65rp9l865vctpj0535kpoh0bfo5pkd0.apps.googleusercontent.com&redirect_uri=http://localhost:8080/rest/redirect/google ";
-  const google_url : String = "http://localhost:8080/rest/myresource/easy";
+  google_auth: boolean = false;
+  //google_url: string = 'https://esir.univ-rennes1.fr/';
+  const google_url: String = "https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&response_type=code&client_id=752520275648-e65rp9l865vctpj0535kpoh0bfo5pkd0.apps.googleusercontent.com&redirect_uri=http://localhost:8080/rest/redirect/google ";
+  //const google_url : String = "http://localhost:8080/rest/myresource/easy";
   constructor(
-    private api : ApiService
+    private api: ApiService
   ) {}
 
   ngOnInit() {
   }
-
 
   getGoogleAccess() {
     this.google_auth=this.api.connectAPI(this.google_url);
