580c1abaab559c3258f30d76e13c75a36bfe26c5
AcidBaz <30378151+AcidBaz@users.noreply.github.com>
2018-05-24 11:36:45 +0100

    deleted file info

--- jxs-front/src/app/app.module.ts
+++ jxs-front/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FilesDisplayComponent } from './files-display/files-display.component';
 import { LateralPanelComponent } from './lateral-panel/lateral-panel.component';
-import { FileInfoComponent } from './file-info/file-info.component';
 import { FichierDetailComponent } from './fichier-detail/fichier-detail.component';
 import { AppRoutingModule } from './app-routing.module';
 import {UploaderComponent} from './uploader/uploader.component'
@@ -20,7 +19,6 @@ import {CookieService} from 'angular2-cookie/core';
     AppComponent,
     FilesDisplayComponent,
     LateralPanelComponent,
-    FileInfoComponent,
     FichierDetailComponent,
     UploaderComponent
   ],
